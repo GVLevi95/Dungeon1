c66e9b4131d3d2c2fc8556023233a2eab4ca8262
Pedro <55149673+Deckerdyn@users.noreply.github.com>
2020-07-23 11:48:15 -0400

    Version para android
    Esta version sirve para poder usarse en dispositivo android.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -21,7 +22,7 @@ public class Player : MovingObject
 
 	private Animator animator;
 	private int food;
-
+	private Vector2 touchOrigin = -Vector2.one;
 	protected override void Start()
 	{
 		animator = GetComponent<Animator>();
@@ -34,12 +35,15 @@ public class Player : MovingObject
 
 	void Update()
 	{
+
 		if (!GameManager.instance.playersTurn) {
 			return;
 		}
 
 		int horizontal = 0;
 		int vertical = 0;
+		//version para android
+	#if UNITY_STANDLINE || UNITY_WEBPLAYER
 
 		horizontal = (int) Input.GetAxisRaw("Horizontal");
 		vertical = (int) Input.GetAxisRaw("Vertical");
@@ -47,6 +51,32 @@ public class Player : MovingObject
 		if (horizontal != 0) {
 			vertical = 0;
 		}
+	#else
+		if (Input.touchCount>0)
+		{
+			Touch myTouch = Input.touches[0];
+
+			if (myTouch.phase == TouchPhase.Began)
+			{
+				touchOrigin = myTouch.position;
+			}
+			else if(myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+			{
+				Vector2 touchEnd = myTouch.position;
+				float x = touchEnd.x - touchOrigin.x;
+				float y = touchEnd.y - touchOrigin.y;
+				touchOrigin.x = -1;
+				if (Mathf.Abs(x) > Mathf.Abs(y))
+				{
+					horizontal = x > 0 ? 1 : -1;
+				}
+				else
+					vertical = y > 0 ? 1 : -1;
+			}
+		}
+		#endif
+
+
 
 		if (horizontal != 0 || vertical != 0) {
 			AttemptMove<Wall>(horizontal, vertical);
@@ -133,3 +163,4 @@ public class Player : MovingObject
 	}
 
 }
+
